e2e527c0a1d86e2b8992ac269dbf05fe827cb791
Faiza Kamilah <18220075@mahasiswa.itb.ac.id>
2022-11-28 17:14:46 +0700

    commit

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-from fastapi import FastAPI
 from models import User
+from pydantic import BaseModel
 from mongoengine import connect
 import json
+import jwt
+from datetime import datetime, timedelta
+from typing import Optional
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
 
 app = FastAPI()
 connect(db="tst", host="localhost", port=27017)
+JWT_SECRET = 'tubestst'
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 @app.get("/")
 def home():
-    return{"message":"Hello World!"}
+    return{"Name":"Faiza Kamilah"}
 
 @app.get("/getUsers")
 def getUsers():
     users = json.loads(User.objects().to_json())
 
     return {"users": users}
+
+class NewUser(BaseModel):
+    username: str
+    password: str
+
+@app.post("/register")
+def register(new_user: NewUser):
+    user = User(username = new_user.username,
+                password = new_user.password)
+    user.save()
+
+    return {"message":"New user created"}
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from mongoengine import Document, StringField, IntField, ListField
 
-
 class User(Document):
-    id = IntField(max_length=32)
+    #id = IntField(max_length=32)
+    #_id = ObjectId()
     username = StringField()
     password = StringField()
+    meta={'allow_inheritance':True}
